b8d37230b487449070f7890c37fd6009ab5f78f9
tomml <tomabel92@protonmail.com>
2024-12-05 23:15:56 +0100

    solution for day 5 star 1

--- PuzzleUtils.java
+++ PuzzleUtils.java
@@ -3,7 +3,9 @@ import java.io.FileReader;
 import java.io.LineNumberReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PuzzleUtils {
     public static String readInputAsString(String filename)
@@ -80,6 +82,63 @@ public class PuzzleUtils {
         }
         catch (Exception e) {
             throw new RuntimeException();
+        }
+    }
+
+    public static Map<Integer, List<Integer>> readInputAsMap(String filename)
+    {
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(filename)))
+        {
+            String line = br.readLine();
+
+            while (line != null)
+            {
+                String[] lineArray = line.split("\\|");
+                //System.out.println(Arrays.toString(lineArray));
+                List<Integer> possibleValue = map.get(Integer.parseInt(lineArray[0]));
+                if (possibleValue != null)
+                {
+                    possibleValue.add(Integer.parseInt(lineArray[1]));
+                    map.put(Integer.parseInt(lineArray[0]), possibleValue);
+                }
+                else
+                {
+                    map.put(Integer.parseInt(lineArray[0]), new ArrayList<Integer>(Arrays.asList(Integer.parseInt(lineArray[1]))));
+                }
+                line = br.readLine();
+            }
+
+            return map;
+        }
+        catch(Exception e) 
+        {
+            e.printStackTrace();
+            return new HashMap<>();
+        }
+    }
+
+    public static List<int[]> readInputAsIntArray(String filename)
+    {
+        List<int[]> list = new ArrayList<>();
+        try(BufferedReader br = new BufferedReader(new FileReader(filename)))
+        {
+            String line = br.readLine();
+
+            while (line != null)
+            {
+                int[] lineArray = Arrays.stream(line.split(",")).mapToInt(Integer::parseInt).toArray();
+                list.add(lineArray);
+                line = br.readLine();
+            }
+
+            return list;
+        }
+        catch(Exception e) 
+        {
+            e.printStackTrace();
+            return new ArrayList<>();
         }
     }
 }
---
+++ day-5/SolutionDay5.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class SolutionDay5 {
+    public static void main(String[] args)
+    {
+        star1();
+    }
+
+    public static void star1()
+    {
+        int solution = 0;
+        Map<Integer, List<Integer>> inputRules = PuzzleUtils.readInputAsMap("C:\\\\Users\\\\thoma\\\\Github\\\\advent-of-code-2024\\\\day-5\\\\input1.txt");
+        List<int[]> inputPages = PuzzleUtils.readInputAsIntArray("C:\\\\Users\\\\thoma\\\\Github\\\\advent-of-code-2024\\\\day-5\\\\input2.txt");
+        //System.out.println(inputRules.toString());
+        //System.out.println(inputPages.toString());
+        outerLoop:
+        for (int[] pageList : inputPages)
+        {
+            //System.out.println(Arrays.toString(pageList));
+            for (int i = 0; i < pageList.length; i++)
+            {
+                List<Integer> pagesBefore = getElementsBefore(pageList, i);
+                boolean check = checkCorrectOrder(pagesBefore, inputRules.get(pageList[i]));
+                if (check != true)
+                {
+                    continue outerLoop;
+                }
+            }
+            solution += addToSolution(pageList);
+        }
+        System.out.println("Ergebnis: " + solution);
+    }
+
+    public static List<Integer> getElementsBefore(int[] pageList, int k)
+    {
+        List<Integer> returnList = new ArrayList<>();
+        for (int i = 0; i < k; i++)
+        {
+            returnList.add(pageList[i]);
+        }
+        return returnList;
+    }
+
+    public static boolean checkCorrectOrder(List<Integer> pagesBefore, List<Integer> rules)
+    {
+        for (Integer i : pagesBefore)
+        {
+            if (rules.contains(i))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int addToSolution(int[] pageList)
+    {
+        int n = pageList.length;
+        int mid = n / 2;
+        return pageList[mid];
+    }
+}
