10b54f8778c2554166b11054efaba6c0a4f1a7b5
Thomas Abel <thomas.abel@exentra.de>
2024-12-09 17:39:33 +0100

    solution for day 9 star 1

--- day_9/SolutionDay9.java
+++ day_9/SolutionDay9.java
@@ -3,6 +3,9 @@ package day_9;
 import utils.PuzzleUtils;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class SolutionDay9
 {
@@ -14,8 +17,8 @@ public class SolutionDay9
     public static void star1()
     {
         String filePath = new File("").getAbsolutePath();
-        String input = PuzzleUtils.readInputAsString(filePath + "/day_9/testinput.txt");
-        String parsedInput = "";
+        String input = PuzzleUtils.readInputAsString(filePath + "/day_9/input.txt");
+        List<String> parsedInputAsList = new ArrayList<>();
         int currentNumber = 0;
 
         for (int i = 0; i < input.length(); i++)
@@ -23,15 +26,48 @@ public class SolutionDay9
             char c = input.charAt(i);
             if (i % 2 == 0)
             {
-                parsedInput += String.valueOf(currentNumber).repeat(Integer.parseInt(String.valueOf(c)));
+                parsedInputAsList.addAll(Collections.nCopies(Integer.parseInt(String.valueOf(c)), String.valueOf(currentNumber)));
                 currentNumber++;
             }
             else
             {
-                parsedInput += ".".repeat(Integer.parseInt(String.valueOf(c)));
+                parsedInputAsList.addAll(Collections.nCopies(Integer.parseInt(String.valueOf(c)), "."));
             }
         }
 
-        System.out.println(parsedInput);
+        int left = 0;
+        int right = parsedInputAsList.size() - 1;
+
+        while (left < right)
+        {
+            if (parsedInputAsList.get(left).equals(".") && !parsedInputAsList.get(right).equals("."))
+            {
+                parsedInputAsList.set(left, parsedInputAsList.get(right));
+                parsedInputAsList.set(right, ".");
+                left++;
+                right--;
+            }
+            else
+            {
+                if (!parsedInputAsList.get(left).equals("."))
+                {
+                    left++;
+                }
+                if (parsedInputAsList.get(right).equals("."))
+                {
+                    right--;
+                }
+            }
+        }
+
+        int index = 0;
+        long solution = 0;
+        while (!parsedInputAsList.get(index).equals("."))
+        {
+            solution += Long.parseLong(parsedInputAsList.get(index)) * index;
+            index++;
+        }
+
+        System.out.println("Ergebnis: " + solution);
     }
 }
