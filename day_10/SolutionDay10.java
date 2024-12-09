0da8b59851112ed15551d61d0413a223ab1ae5b7
tomml <tomabel92@protonmail.com>
2024-12-09 23:44:44 +0100

    prepare for day 10

---
+++ day_10/SolutionDay10.java
@@ -0,0 +1,14 @@
+package day_10;
+
+public class SolutionDay10
+{
+    public static void main(String[] args)
+    {
+        star1();
+    }
+
+    public static void star1()
+    {
+        //
+    }
+}
