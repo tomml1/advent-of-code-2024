e20034f7d73ce18934e7b76d21df5ff2a85063f1
tomml <tomabel92@protonmail.com>
2024-12-16 23:22:20 +0100

    solution for day 13 star 1

--- day_13/SolutionDay13.java
+++ day_13/SolutionDay13.java
@@ -14,8 +14,40 @@ public class SolutionDay13
     public static void star1()
     {
         List<Day13SingleInput> input = getInput();
-        //should be 320
-        System.out.println(input.size());
+
+        int tokenPrizeA = 3,
+            tokenPrizeB = 1;
+        
+        int solution = 0;
+
+        for(Day13SingleInput singleInput : input)
+        {
+            int cost = Integer.MAX_VALUE;
+            for (int i = 0; i < 101; i++)
+            {
+                for (int j = 0; j < 101; j++)
+                {
+                    int resX = i*singleInput.ax + j*singleInput.bx;
+                    int resY = i*singleInput.ay + j*singleInput.by;
+                    
+                    if (resX == singleInput.px && resY == singleInput.py)
+                    {
+                        int currentCost = tokenPrizeA * i + tokenPrizeB * j;
+                        if (currentCost < cost)
+                        {
+                            cost = currentCost;
+                        }
+                    }
+                }
+            }
+
+            if (cost < Integer.MAX_VALUE)
+            {
+                solution += cost;
+            }
+        }
+
+        System.out.println(solution);
     }
 
     public static List<Day13SingleInput> getInput()
