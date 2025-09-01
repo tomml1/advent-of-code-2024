f1abd7620f20a4c0468095d9b47026dd391777ba
tabel <thomas.abel@soprasteria.com>
2025-09-01 17:43:27 +0200

    🚑️ it's moving!

--- day_15/SolutionDay15.java
+++ day_15/SolutionDay15.java
@@ -13,10 +13,10 @@ public class SolutionDay15
         star1();
     }
 
-    public static void star1()
+    public static void star1(String fileNameMaze, String fileNameMoves)
     {
-        String[][] maze = getMaze();
-        String[] moves = getMoves();
+        String[][] maze = getMaze(fileNameMaze);
+        String[] moves = getMoves(fileNameMoves);
 
         int rowNumber = maze.length;
         int columnNumber = maze[0].length;
@@ -82,16 +82,39 @@ public class SolutionDay15
         }
     }
 
-    public static String[][] getMaze()
+    public static void visualizeBetter(String[][] maze)
     {
-        String filePath = new File("").getAbsolutePath();
-        return PuzzleUtils.readInputAs2DArray(filePath + "/day_15/inputmace.txt");
+        int rowNumber = maze.length;
+        for (int i = 0; i < rowNumber; i++)
+        {
+            String row = "";
+            for (int j = 0; j < maze[0].length; j++)
+            {
+                row += maze[i][j];
+            }
+            System.out.println(row);
+        }
+
+        try
+        {
+            TimeUnit.SECONDS.sleep(1);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 
-    public static String[] getMoves()
+    public static String[][] getMaze(String filename)
     {
         String filePath = new File("").getAbsolutePath();
-        return PuzzleUtils.readInputAsString(filePath + "/day_15/inputmoves.txt").split("");
+        return PuzzleUtils.readInputAs2DArray(filePath + "/day_15/" + filename);
+    }
+
+    public static String[] getMoves(String filename)
+    {
+        String filePath = new File("").getAbsolutePath();
+        return PuzzleUtils.readInputAsString(filePath + "/day_15/" + filename).split("");
     }
 
     public static Point getStartPosition(String[][] maze)
@@ -244,6 +267,169 @@ public class SolutionDay15
                 maze[posY][posX - 1] = "@";
                 position.setLocation(posX - 1, posY);
             }
+        }
+    }
+
+    public static void star2()
+    {
+        String[][] maze = getMaze("customtestinputmace.txt");
+        String[] moves = getMoves("customtestinputmoves.txt");
+        String[][] expandedMaze = expandMaze(maze);
+        visualizeBetter(expandedMaze);
+
+        Point startPosition = getStartPosition(expandedMaze);
+
+        Boolean possible = true;
+
+        for (var move : moves)
+        {
+            System.out.println("current move: " + move);
+            if (move.equals("<") || move.equals(">"))
+            {
+                String[][] testMaze = expandedMaze.clone();
+                moveBoxesHorizontally(startPosition, move, testMaze, possible);
+                if(possible)
+                {
+                    expandedMaze = testMaze;
+                }
+            }
+            else if (move.equals("^") || move.equals("v"))
+            {
+                String[][] testMaze = expandedMaze.clone();
+                moveBoxesRecursively(startPosition, move, testMaze, possible);
+                if(possible)
+                {
+                    expandedMaze = testMaze;
+                }
+            }
+            possible = true;
+            visualizeBetter(expandedMaze);
+        }
+    }
+
+    public static String[][] expandMaze(String[][] maze)
+    {
+        String[][] expandedMaze = new String[maze.length][maze[0].length * 2];
+        for (int i = 0; i < maze.length; i++)
+        {
+            for (int j = 0; j < maze[0].length; j++)
+            {
+                if (maze[i][j].equals("#"))
+                {
+                    expandedMaze[i][j*2] = "#";
+                    expandedMaze[i][j*2 + 1] = "#";
+                }
+                if (maze[i][j].equals("O"))
+                {
+                    expandedMaze[i][j*2] = "[";
+                    expandedMaze[i][j*2 + 1] = "]";
+                }
+                if (maze[i][j].equals("."))
+                {
+                    expandedMaze[i][j*2] = ".";
+                    expandedMaze[i][j*2 + 1] = ".";
+                }
+                if (maze[i][j].equals("@"))
+                {
+                    expandedMaze[i][j*2] = "@";
+                    expandedMaze[i][j*2 + 1] = ".";
+                }
+            }
+        }
+        return expandedMaze;
+    }
+
+    public static boolean isBox(Point position, String[][] maze)
+    {
+        int posX = (int) position.getX();
+        int posY = (int) position.getY();
+
+        if (maze[posY][posX].equals("[") || maze[posY][posX].equals("]"))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static void moveBoxesHorizontally(Point position, String direction, String[][] maze, Boolean possible) {
+        int directionIncrement;
+        if (direction.equals("<"))
+        {
+            directionIncrement = -1;
+        } else if
+        (direction.equals(">")) {
+            directionIncrement = 1;
+        } else
+        {
+            throw new IllegalArgumentException();
+        }
+
+        int posX = (int) position.getX();
+        int posY = (int) position.getY();
+
+        if (maze[posY][posX + directionIncrement].equals("[") || maze[posY][posX + directionIncrement].equals("]"))
+        {
+            moveBoxesHorizontally(new Point(posX + directionIncrement, posY), direction, maze, possible);
+        }
+
+        if (maze[posY][posX + directionIncrement].equals("."))
+        {
+            maze[posY][posX + directionIncrement] = maze[posY][posX];
+            maze[posY][posX] = ".";
+            position.setLocation(posX + directionIncrement, posY);
+            return;
+        }
+        else if (maze[posY][posX + directionIncrement].equals("#"))
+        {
+            possible = false;
+            return;
+        }
+    }
+
+    public static void moveBoxesRecursively(Point position, String direction, String[][] maze, Boolean possible)
+    {
+        //BFS to determine if moving is possible
+        int directionIncrement;
+        if (direction.equals("^"))
+        {
+            directionIncrement = -1;
+        }
+        else if (direction.equals("v"))
+        {
+            directionIncrement = 1;
+        }
+        else
+        {
+            throw new IllegalArgumentException();
+        }
+
+        int posX = (int) position.getX();
+        int posY = (int) position.getY();
+
+
+        if (maze[posY + directionIncrement][posX].equals("["))
+        {
+            moveBoxesRecursively(new Point(posX, posY + directionIncrement), direction, maze, possible);
+            moveBoxesRecursively(new Point(posX + 1, posY + directionIncrement), direction, maze, possible);
+        }
+        else if (maze[posY + directionIncrement][posX].equals("]"))
+        {
+            moveBoxesRecursively(new Point(posX, posY + directionIncrement), direction, maze, possible);
+            moveBoxesRecursively(new Point(posX - 1, posY + directionIncrement), direction, maze, possible);
+        }
+
+        if (maze[posY + directionIncrement][posX].equals("."))
+        {
+            maze[posY + directionIncrement][posX] = maze[posY][posX];
+            maze[posY][posX] = ".";
+            position.setLocation(posX, posY + directionIncrement);
+            return;
+        }
+        else if (maze[posY + directionIncrement][posX].equals("#"))
+        {
+            possible = false;
+            return;
         }
     }
 }
